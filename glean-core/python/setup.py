656442f60c919c3c7b86f6142ea1ccc5a4a30c6e
Jan-Erik Rediger <jrediger@mozilla.com>
2022-06-08 13:10:41 +0200

    Relax glean_parser version requirement to "all compatible releases"
    In pip `~=V.N` is equivalent to `>= V.N, == V.*`
    So we essentially allow everything from the specified minor version
    upwards until the next major version (excluded).
    As long as glean_parser is careful to not introduce breaking changes
    before that this will simplify work a lot by allowing small bug fixes
    without requiring subsequent Glean releases to bump versions all the
    time.

--- glean-core/python/setup.py
+++ glean-core/python/setup.py
@@ -59,7 +59,8 @@ with (SRC_ROOT / "CHANGELOG.md").open() as history_file:
 version = "50.1.2"
 
 requirements = [
-    "glean_parser==6.1.1",
+    "semver>=2.13.0",
+    "glean_parser~=6.1",
 ]
 
 # The environment variable `GLEAN_BUILD_VARIANT` can be set to `debug` or `release`
